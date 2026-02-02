89cfc591b977615c638954ad0427be57ed47bf47
Charles <cia.velasco.2024@smu.edu.sg>
2026-02-02 22:15:25 +0800

    fix: add suspense boundary to verify-otp page

--- src/app/verify-otp/page.tsx
+++ src/app/verify-otp/page.tsx
@@ -1,11 +1,12 @@
+import { Suspense } from "react"; // <--- 1. Import Suspense
 import { OTPForm } from "@/components/auth/otp-form";
 
-export default function OTPPage() {
+export default function VerifyOtpPage() {
     return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm">
+        <div className="flex h-screen w-full items-center justify-center px-4">
+            <Suspense>
                 <OTPForm />
-            </div>
+            </Suspense>
         </div>
     );
 }
