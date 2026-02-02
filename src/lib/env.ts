a78384ab8588385814b5ff22996cfa6214d4e570
Charles <cia.velasco.2024@smu.edu.sg>
2026-02-02 21:19:46 +0800

    feat: added new env variables

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,39 +2,44 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-  client: {
-    VITE_APP_URL: z.url(),
-  },
-  server: {
-    NEXT_PUBLIC_SUPABASE_URL: z.string(),
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    client: {
+        NEXT_PUBLIC_APP_URL: z.url().default("http://localhost:3000"),
+    },
+    server: {
+        GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
+        NODE_ENV: z.enum(["development", "production"]).default("development"),
 
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-    CLERK_SECRET_KEY: z.string(),
+        DATABASE_URL: z.string(),
 
-    GOOGLE_GENERATIVE_AI_API_KEY: z.string(),
-    NODE_ENV: z.enum(["development", "production"]).default("development"),
-  },
-  clientPrefix: "VITE_",
-  runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-    VITE_APP_URL: process.env.VITE_APP_URL ?? `http://localhost:4000`,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        GOOGLE_CLIENT_ID: z.string(),
+        GOOGLE_CLIENT_SECRET: z.string(),
 
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+        BETTER_AUTH_URL: z.url().default("http://localhost:3000"),
+        BETTER_AUTH_SECRET: z.string(),
 
-    GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+        RESEND_API_KEY: z.string(),
+    },
+    clientPrefix: "NEXT_PUBLIC_",
+    runtimeEnv: {
+        DATABASE_URL: process.env.DATABASE_URL,
 
-    // Better Auth (leave this here to migrate to better-auth)
-    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-  },
-  skipValidation:
-    !!process.env.CI || process.env.npm_lifecycle_event === "lint",
+        NODE_ENV: process.env.NODE_ENV,
+        NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+
+        GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+        BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+
+        RESEND_API_KEY: process.env.RESEND_API_KEY,
+
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    skipValidation:
+        !!process.env.CI || process.env.npm_lifecycle_event === "lint",
 });
 
 export type Env = {
-  [K in keyof typeof env as K extends `VITE_${string}` ? K : never]: string;
+    [K in keyof typeof env as K extends `VITE_${string}` ? K : never]: string;
 };
