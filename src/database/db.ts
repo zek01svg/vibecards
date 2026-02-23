1b401f2a34fed4a2ee42f037fac162f78e1152a1
Charles <zekvelasco15@gmail.com>
2026-02-24 00:29:13 +0800

    chore: removed test db call

--- src/database/db.ts
+++ src/database/db.ts
@@ -10,18 +10,6 @@ const pool = new Pool({
   connectionString: env.DATABASE_URL,
 });
 
-// test connection first before exporting
-(async () => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT 1 + 1 AS test");
-    client.release();
-    logger.info({ test: result.rows[0] }, "Database connection successful");
-  } catch (err) {
-    logger.error({ err }, "Database connection failed");
-  }
-})();
-
 const db = drizzle(pool, { schema });
 
 export default db;
