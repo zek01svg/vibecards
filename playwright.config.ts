91a0668eeaf0b715f6dd52d2ec6647668adffceb
Charles <zekvelasco15@gmail.com>
2026-02-24 02:10:15 +0800

    fix: added dotenv to load env vars

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,8 @@
+import path from "path";
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
 
 export default defineConfig({
   testDir: "./tests",
