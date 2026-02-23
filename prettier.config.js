ac328a31120337c9f00363d84b4dc4ab4435f0cb
Charles <zekvelasco15@gmail.com>
2026-02-23 17:03:55 +0800

    fix: removed tabwidth setting causing indentation errors

--- prettier.config.js
+++ prettier.config.js
@@ -4,41 +4,40 @@
 
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const prettierConfig = {
-    tabWidth: 4,
-    plugins: [
-        "@ianvs/prettier-plugin-sort-imports",
-        "prettier-plugin-tailwindcss",
-    ],
-    tailwindFunctions: ["cn", "cva"],
-    importOrder: [
-        "<TYPES>",
-        "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
-        "^(next/(.*)$)|^(next$)",
-        "^(expo(.*)$)|^(expo$)",
-        "<THIRD_PARTY_MODULES>",
-        "",
-        "<TYPES>^[.|..|~]",
-        "^#client/",
-        "^#server/",
-        "^[../]",
-        "^[./]",
-    ],
-    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-    importOrderTypeScriptVersion: "4.4.0",
-    overrides: [
-        {
-            files: "*.json.hbs",
-            options: {
-                parser: "json",
-            },
-        },
-        {
-            files: "*.js.hbs",
-            options: {
-                parser: "babel",
-            },
-        },
-    ],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindFunctions: ["cn", "cva"],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(expo(.*)$)|^(expo$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^#client/",
+    "^#server/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
+  overrides: [
+    {
+      files: "*.json.hbs",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.js.hbs",
+      options: {
+        parser: "babel",
+      },
+    },
+  ],
 };
 
 export default prettierConfig;
