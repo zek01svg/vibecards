e0d1bde5270be43870facb154537d3bf69f46f1a
Charles <zekvelasco15@gmail.com>
2026-02-23 13:46:26 +0800

    feat: created signup button with spinner

---
+++ src/components/ui/signup-button.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+import { Button } from "./button";
+import { FieldDescription } from "./field";
+import { Spinner } from "./spinner";
+
+interface SignupButtonProps {
+  isSubmitting: boolean;
+}
+
+export default function SignupButton({ isSubmitting }: SignupButtonProps) {
+  return (
+    <>
+      <Button type="submit" disabled={isSubmitting}>
+        {isSubmitting ? (
+          <>
+            &quot;Creating Account...&quot;
+            <Spinner className="ml-2" />
+          </>
+        ) : (
+          <>&quot;Create Account&quot;</>
+        )}
+      </Button>
+      <FieldDescription className="text-center">
+        Already have an account? <Link href="/sign-in">Sign in</Link>
+      </FieldDescription>
+    </>
+  );
+}
