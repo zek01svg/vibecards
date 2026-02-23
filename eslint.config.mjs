0900ca4b4fff6f947fa7122d7807166b41efb087
Charles <zekvelasco15@gmail.com>
2026-02-23 17:05:21 +0800

    fix: fixed react version mismatch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,61 @@
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 export const restrictEnvAccess = tseslint.config(
-    { ignores: ["**/env.ts", 
-        "**/playwright.config.ts",
-        "**/instrumentation.ts",
-        "**/instrumentation-client.ts",
-        "**/instrumentation-server.ts",
-        "**/instrumentation-edge.ts",
-        "**/next.config.js"] },
-    {
-        files: ["**/*.js", "**/*.ts", "**/*.tsx"],
-        rules: {
-            "no-restricted-properties": [
-                "error",
-                {
-                    object: "process",
-                    property: "env",
-                    message:
-                        "Use `import { env } from '@/lib/env'` instead to ensure validated types.",
-                },
-            ],
-            "no-restricted-imports": [
-                "error",
-                {
-                    name: "process",
-                    importNames: ["env"],
-                    message:
-                        "Use `import { env } from '@/lib/env'` instead to ensure validated types.",
-                },
-            ],
+  {
+    ignores: [
+      "**/env.ts",
+      "**/playwright.config.ts",
+      "**/instrumentation.ts",
+      "**/instrumentation-client.ts",
+      "**/instrumentation-server.ts",
+      "**/instrumentation-edge.ts",
+      "**/next.config.js",
+    ],
+  },
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-restricted-properties": [
+        "error",
+        {
+          object: "process",
+          property: "env",
+          message:
+            "Use `import { env } from '@/lib/env'` instead to ensure validated types.",
         },
-    }
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          name: "process",
+          importNames: ["env"],
+          message:
+            "Use `import { env } from '@/lib/env'` instead to ensure validated types.",
+        },
+      ],
+    },
+  },
 );
 
 const eslintConfig = defineConfig([
-    ...nextVitals,
-    ...restrictEnvAccess,
-    globalIgnores([
-        ".next/**",
-        "out/**",
-        "build/**",
-        "next-env.d.ts",
-        "node_modules/**",
-    ]),
+  ...nextVitals,
+  ...restrictEnvAccess,
+  {
+    settings: {
+      react: {
+        version: "19.2.4",
+      },
+    },
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "node_modules/**",
+  ]),
 ]);
 
 export default eslintConfig;
