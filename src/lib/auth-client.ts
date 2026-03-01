ff2cd5efd73d561e754f6829d830667aaaf4761c
Charles <zekvelasco15@gmail.com>
2026-03-01 15:55:45 +0800

    fix: import from better auth react library

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
-import { createAuthClient } from "better-auth/client";
+import { createAuthClient } from "better-auth/react";
 import { emailOTPClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    plugins: [emailOTPClient()],
+  plugins: [emailOTPClient()],
 });
