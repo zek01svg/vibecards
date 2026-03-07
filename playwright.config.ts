c9482dd28233b3c9cc4779ad6277c29d80bd690b
Charles <zekvelasco15@gmail.com>
2026-03-07 15:52:58 +0800

    chore: added `quiet:true` to dotenv.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
 
-dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local"), quiet: true });
 
 export default defineConfig({
   testDir: "./tests/e2e",
