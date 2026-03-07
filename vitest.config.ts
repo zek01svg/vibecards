afc81ca53717abdc7514ff6e91573b8eaa391073
Charles <zekvelasco15@gmail.com>
2026-03-07 12:52:36 +0800

    fix: excluded e2e
    chore: added html and dot reporters

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,29 +1,38 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig, defineProject, mergeConfig } from "vitest/config";
 
 export const baseConfig = defineConfig({
-    test: {
-        coverage: {
-            provider: "istanbul" as const,
-            reporter: [
-                [
-                    "json",
-                    {
-                        file: `../coverage.json`,
-                    },
-                ],
-            ] as const,
-            enabled: true,
-        },
+  test: {
+    exclude: [
+      "node_modules",
+      "coverage",
+      "dist",
+      ".next",
+      "playwright",
+      "tests/e2e",
+    ],
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+    coverage: {
+      provider: "istanbul" as const,
+      reporter: [
+        ["json", { subdir: "json" }],
+        ["html", { subdir: "html" }],
+      ] as const,
+      enabled: true,
+    },
+    reporters: ["dot"],
+  },
 });
 
 const uiConfig = mergeConfig(
-    baseConfig,
-    defineProject({
-        test: {
-            environment: "jsdom",
-        },
-    }),
+  baseConfig,
+  defineProject({
+    test: {
+      environment: "jsdom",
+    },
+  }),
 );
 
 export default uiConfig;
